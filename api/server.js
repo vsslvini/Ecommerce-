62e3b73c80b2c1232a1e0aee77649079cdfc473f
vsslvini <vsslviniciussousa@gmail.com>
2024-01-30 16:15:55 -0300

    Projeto inicial

---
+++ api/server.js
@@ -0,0 +1,62 @@
+//PACOTES
+const compressiion = require("compression");
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+
+//START
+const app = express();
+
+//AMBIENTE 
+const isProduction = process.env.NODE_ENV === "production";
+const PORT = process.env.PORT || 3000;
+
+//ARQUIVO ESTATICOS
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/public/images", express.static(__dirname + "/public/images"));
+
+// SETUP MONGODB
+const dbs = require("./config/database");
+const dbURI = isProduction ? dbs.dbProduction : dbs.dbTeste;
+mongoose.connect(dbURI, { useNewUrlParser: true });
+
+//SETUP EJS
+app.set("view engine", "ejs");
+
+//CONFIGURACOES 
+if (!isProduction) app.use(morgan("dev"));
+app.use(cors());
+app.disable("x-powered-by");
+app.use(compressiion());
+
+//SETUP BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false, limit: 1.5 * 1024 * 1024 }));
+app.use(bodyParser.json({ limit: 1.5 * 1024 * 1024 }));
+
+//MODELS
+require("./models");
+//ROTAS
+app.use("./", require("./routes"));
+
+//404 - ROTA
+app.use((req, res, next) => {
+    const err = new Error("Not Foud");
+    err.status = 404;
+    next(err);
+});
+
+// ROTA - 422 - 500 - 401
+app.use((err, req, next) => {
+    res.status(err.status || 500);
+    if (err.status !== 404) console.warn("Erro: ", err.message, new Date());
+    res.json({ errors: { message: err.message, status: err.status } });
+});
+
+// ESCUTAR
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log('Rodando na //localhost:${PORT}');
+});
